cac08aed42f918b8b21ce8964dfd4317071a88b0
arandombit <a.random.luis@proton.me>
2025-05-19 19:45:07 -0400

    organize stuff into src

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,10 @@
-import * as React from 'react';
-import { View } from 'react-native';
-import Animated, { FadeInUp, FadeOutDown, LayoutAnimationConfig } from 'react-native-reanimated';
-import { Info } from '~/lib/icons/Info';
-import { Avatar, AvatarFallback, AvatarImage } from '~/components/ui/avatar';
-import { Button } from '~/components/ui/button';
+import * as React from 'react'
+import { View } from 'react-native'
+import Animated, { FadeInUp, FadeOutDown, LayoutAnimationConfig } from 'react-native-reanimated'
+
+import { Info } from '~/lib/icons/Info'
+import { Avatar, AvatarFallback, AvatarImage } from '~/reusables/ui/avatar'
+import { Button } from '~/reusables/ui/button'
 import {
   Card,
   CardContent,
@@ -11,19 +12,19 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from '~/components/ui/card';
-import { Progress } from '~/components/ui/progress';
-import { Text } from '~/components/ui/text';
-import { Tooltip, TooltipContent, TooltipTrigger } from '~/components/ui/tooltip';
+} from '~/reusables/ui/card'
+import { Progress } from '~/reusables/ui/progress'
+import { Text } from '~/reusables/ui/text'
+import { Tooltip, TooltipContent, TooltipTrigger } from '~/reusables/ui/tooltip'
 
 const GITHUB_AVATAR_URI =
-  'https://i.pinimg.com/originals/ef/a2/8d/efa28d18a04e7fa40ed49eeb0ab660db.jpg';
+  'https://i.pinimg.com/originals/ef/a2/8d/efa28d18a04e7fa40ed49eeb0ab660db.jpg'
 
 export default function Screen() {
-  const [progress, setProgress] = React.useState(78);
+  const [progress, setProgress] = React.useState(78)
 
   function updateProgressValue() {
-    setProgress(Math.floor(Math.random() * 100));
+    setProgress(Math.floor(Math.random() * 100))
   }
   return (
     <View className='flex-1 justify-center items-center gap-5 p-6 bg-secondary/30'>
@@ -91,5 +92,5 @@ export default function Screen() {
         </CardFooter>
       </Card>
     </View>
-  );
+  )
 }
