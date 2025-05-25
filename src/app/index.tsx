c07d8815314973c0a2fd19b65e411a65e49569f0
arandombit <a.random.luis@proton.me>
2025-05-24 22:28:45 -0400

    add input element to receive ISBN information

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,20 @@
 import * as React from 'react'
 import { View } from 'react-native'
+
 import { Text } from '~/reusables/ui/text'
+import { Input } from '~/reusables/ui/input'
 
 export default function Screen() {
+  const [isbn, setIsbn] = React.useState('')
+
   return (
-    <View className='flex-1 justify-center items-center gap-5 p-6 bg-secondary/30'>
-      <Text>Hello!</Text>
+    <View className='flex-1 justify-start items-center gap-5 p-6 bg-secondary/30'>
+      <Input
+        className='w-full'
+        value={isbn}
+        onChangeText={setIsbn}
+        placeholder="Enter ISBN here..."
+      />
     </View>
   )
 }
