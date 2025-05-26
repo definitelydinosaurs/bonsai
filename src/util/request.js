3137383c688b9aa7a500ce23803136064d8f5465
arandombit <a.random.luis@proton.me>
2025-05-26 02:41:04 -0400

    fix reference error in request util

--- src/util/request.js
+++ src/util/request.js
@@ -9,7 +9,7 @@ const res = res => res.ok
   : Promise.reject(new Error(res))
 
 export default {
-  get: (url, headers = {}) => fetch(url, { headers, agent }).then(res),
+  get: (url, headers = {}) => fetch(url, { headers }).then(res),
   ...['delete', 'patch', 'post', 'put'].reduce((acc, method) => ({
     ...acc,
     [method]: (url, body, headers = {}) => fetch(url, metadata(method, body, headers))
