d94792e2c63b679523fda6dfec356b40eb891a29
arandombit <a.random.luis@proton.me>
2025-05-26 02:21:12 -0400

    add request util to make network requests

---
+++ src/util/request.js
@@ -0,0 +1,18 @@
+const metadata = (method, data, headers = {}) => ({
+  method,
+  body: JSON.stringify(data),
+  headers: { 'Content-Type': 'application/json', ...headers }
+})
+
+const res = res => res.ok
+  ? res.json().then(d => d).catch(() => res.status)
+  : Promise.reject(new Error(res))
+
+export default {
+  get: (url, headers = {}) => fetch(url, { headers, agent }).then(res),
+  ...['delete', 'patch', 'post', 'put'].reduce((acc, method) => ({
+    ...acc,
+    [method]: (url, body, headers = {}) => fetch(url, metadata(method, body, headers))
+      .then(res)
+  }), {})
+}
