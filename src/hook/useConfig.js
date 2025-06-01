d758d72ba3058aed9bf241c4731688701fe48059
arandombit <a.random.luis@proton.me>
2025-05-31 22:15:37 -0400

    add config.json to source control

--- src/hook/useConfig.js
+++ src/hook/useConfig.js
@@ -1,6 +1,6 @@
 import { useMemo } from 'react'
 
-import config from '../config.json'
+import config from '../../config.json'
 
 export const useConfig = () => {
   const configuration = useMemo(() => config, [])
