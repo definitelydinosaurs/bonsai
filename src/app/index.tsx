612793d3c5c56f9649d4aa8f321f0998760b4798
arandombit <a.random.luis@proton.me>
2025-06-12 11:47:16 +0200

    style button & ensure it only displays for book searches

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -24,7 +24,7 @@ export default function Screen() {
   const book = extractBook(data[Object.keys(data)[0]] || {})
 
   return (
-    <View className='flex-1 justify-start items-center gap-5 p-6 bg-secondary/30'>
+    <View className='flex-1 justify-start items-center gap-5 p-6'>
       <Input
         className='w-full'
         value={isbn}
@@ -36,19 +36,21 @@ export default function Screen() {
       { isLoading && <Text>Loading...</Text> }
       { error && <Text className='text-red-500'>{ error.message }</Text> }
       { Object.keys(data).length > 0 && (
-        <View className='flex-1 h-full w-full pt-[20%]'>
-          <Image
-            source={{ uri: book.cover ? book.cover.large : 'https://i.imgur.com/of4baFL.png' }}
-            className='w-64 h-96 self-center mb-4'
-            resizeMode='contain'
-          />
-          <Text className='text-lg font-bold text-center'>{ book.title }</Text>
-          <Text className='text-center'>{ book.authors?.map(author => author.name).join(', ') }</Text>
-        </View>
+        <>
+          <View className='flex-1 h-full w-full pt-[20%]'>
+            <Image
+              source={{ uri: book.cover ? book.cover.large : 'https://i.imgur.com/of4baFL.png' }}
+              className='w-64 h-96 self-center mb-4'
+              resizeMode='contain'
+            />
+            <Text className='text-lg font-bold text-center'>{ book.title }</Text>
+            <Text className='text-center'>{ book.authors?.map(author => author.name).join(', ') }</Text>
+            <Button variant='outline' className='self-center w-[50%] mt-4' onPress={() => { invoke('dispatch', { event: 'add_source', payload: JSON.stringify(book) }) }}>
+              <Text>Add</Text>
+            </Button>
+          </View>
+        </>
       ) }
-      <Button variant='default' onPress={() => { invoke('dispatch', { event: 'add_source', payload: JSON.stringify(book) }) }}>
-        <Text>Add</Text>
-      </Button>
     </View>
   )
 }
