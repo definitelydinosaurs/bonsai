0fe2029c299ecfe09561574d3366bcb2fad48bb9
arandombit <a.random.luis@proton.me>
2025-06-18 22:24:26 +0200

    initialize shelf data in shelves screen

--- src/app/shelves.jsx
+++ src/app/shelves.jsx
@@ -1,8 +1,15 @@
 import { View } from 'react-native'
+import { invoke } from '@tauri-apps/api/core'
+import { useQuery } from '@tanstack/react-query'
 
 import { Text } from '~/reusables/ui/text'
 
 export default function Settings() {
+  const { data = {} } = useQuery({
+    queryKey: ['shelves'],
+    queryFn: () => invoke('dispatch', { event: 'initialize_data' })
+  })
+
   return (
     <View className='flex-1 p-4'>
       <Text className='text-2xl font-bold mb-4'>Shelves</Text>
