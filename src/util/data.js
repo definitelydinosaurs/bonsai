7b7bc44adb254ca62ea60d8f10cc56e7a9529ec2
arandombit <a.random.luis@proton.me>
2025-07-15 20:57:42 +0200

    add method to check if string contains DOI

--- src/util/data.js
+++ src/util/data.js
@@ -6,3 +6,8 @@ export const extractBook = ({ isbn, title, subtitle, authors = [], cover, descri
   authors: authors.map(author => author.name),
   description
 })
+
+export const isDOI = string => {
+  const regex = /\b(10[.][0-9]{4,}(?:[.][0-9]+)*\b)/g
+  return regex.test(string)
+}
