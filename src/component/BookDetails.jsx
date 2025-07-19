84d1cd49a9a42488bff2e429cd25d6a9cd4c1182
arandombit <a.random.luis@proton.me>
2025-07-19 22:42:40 +0200

    add book authors to details view

--- src/component/BookDetails.jsx
+++ src/component/BookDetails.jsx
@@ -5,6 +5,7 @@ import { Text } from '~/reusables/ui/text'
 const BookDetails = props => (
   <View className='flex-1 items-center'>
     <Text>{ props.title }</Text>
+    <Text>{ props.authors.join(', ') }</Text>
   </View>
 )
 
