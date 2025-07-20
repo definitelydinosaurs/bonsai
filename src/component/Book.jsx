11fb37e459a22f8410018700657d9e3fa7d91b06
arandombit <a.random.luis@proton.me>
2025-07-20 22:48:10 +0200

    use isbn instead of id for book selection

--- src/component/Book.jsx
+++ src/component/Book.jsx
@@ -5,12 +5,12 @@ import { Text } from '~/reusables/ui/text'
 const Book = props => (
   <View className='flex-1 w-full items-center'>
     <Image
-      onPress={() => props.onPress(props.id)}
+      onPress={() => props.onPress(props.isbn)}
       className='w-full max-w-[65%] min-w-[65%] aspect-[2/3] mb-4'
       source={{ uri: props.cover.length > 0 ? props.cover : 'https://i.imgur.com/of4baFL.png' }}
     />
     <Text
-      onPress={() => props.onPress(props.id)}
+      onPress={() => props.onPress(props.isbn)}
       className='text-lg font-bold text-center'
     >
       { props.title }
@@ -18,7 +18,7 @@ const Book = props => (
     { props.subtitle &&
       <Text
         className='text-lg font-bold text-center'
-        onPress={() => props.onPress(props.id)}
+        onPress={() => props.onPress(props.isbn)}
       >
         { props.subtitle }
       </Text>
