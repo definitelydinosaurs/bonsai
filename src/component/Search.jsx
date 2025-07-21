7f3e08abb04cd028dddf8f936e0e5aa20bfa9181
arandombit <a.random.luis@proton.me>
2025-07-21 23:00:00 +0200

    create modal search component

---
+++ src/component/Search.jsx
@@ -0,0 +1,13 @@
+import { View } from 'react-native'
+
+import { Button } from '~/reusables/ui/button'
+import { Text } from '~/reusables/ui/text'
+import { Input } from '~/reusables/ui/input'
+
+const Search = props => (
+  <View>
+    <Text>Search</Text>
+  </View>
+)
+
+export default Search
