710d80361ced02a5133be690599382ffd12e44aa
arandombit <a.random.luis@proton.me>
2025-07-22 18:07:56 +0200

    extract search modal guts into separate component

--- src/component/Search.jsx
+++ src/component/Search.jsx
@@ -4,10 +4,36 @@ import { Button } from '~/reusables/ui/button'
 import { Text } from '~/reusables/ui/text'
 import { Input } from '~/reusables/ui/input'
 
+import Book from '~/component/Book'
+
 const Search = props => (
-  <View>
-    <Text>Search</Text>
-  </View>
+  <>
+    <View className='w-full'>
+      <Input
+        value={props.text}
+        onChangeText={props.setText}
+        placeholder='Enter ISBN here...'
+        onSubmitEditing={props.onSearch}
+        returnKeyType='search'
+      />
+    </View>
+    { props.isLoading && <Text className='w-full mt-4 text-center'>Loading...</Text> }
+    { props.error && <Text className='w-full mt-4 text-center text-red-500'>{ props.error.message }</Text> }
+    { props.isAdded && <Text className='w-full mt-4 text-center text-green-500'>Added</Text> }
+
+    { props.isSuccess && props.text.length > 0 &&
+      <View className='w-full mt-4'>
+        <Book {...props.book} />
+        <Button
+          variant='outline'
+          className='w-full mt-4'
+          onPress={props.onPress}
+        >
+          <Text>{ props.isPending ? 'Adding...' : 'Add to Library' }</Text>
+        </Button>
+      </View>
+    }
+  </>
 )
 
 export default Search
