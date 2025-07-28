03524e794b693ef0deb62ee770e8f46d7c333077
arandombit <a.random.luis@proton.me>
2025-07-28 23:05:59 +0200

    display book cover when displaying book details

--- src/component/BookDetails.jsx
+++ src/component/BookDetails.jsx
@@ -1,9 +1,13 @@
-import { View } from 'react-native'
+import { Image, View } from 'react-native'
 
 import { Text } from '~/reusables/ui/text'
 
 const BookDetails = props => (
   <View className='flex-1 items-center'>
+    <Image
+      className='w-full max-w-[65%] min-w-[65%] aspect-[2/3] mb-4'
+      source={{ uri: props.cover.length > 0 ? props.cover : 'https://i.imgur.com/of4baFL.png' }}
+    />
     <Text>{ props.title }</Text>
     <Text>{ props.authors.join(', ') }</Text>
   </View>
