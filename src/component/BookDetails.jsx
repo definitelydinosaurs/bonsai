0339f9394047c972d08bb77ead1363e24da96bcd
arandombit <a.random.luis@proton.me>
2025-07-29 22:05:47 +0200

    improve book details modal styling

--- src/component/BookDetails.jsx
+++ src/component/BookDetails.jsx
@@ -3,13 +3,14 @@ import { Image, View } from 'react-native'
 import { Text } from '~/reusables/ui/text'
 
 const BookDetails = props => (
-  <View className='flex-1 items-center'>
+  <View className='flex-1 w-full items-center'>
     <Image
       className='w-full max-w-[65%] min-w-[65%] aspect-[2/3] mb-4'
       source={{ uri: props.cover.length > 0 ? props.cover : 'https://i.imgur.com/of4baFL.png' }}
     />
-    <Text>{ props.title }</Text>
-    <Text>{ props.authors.join(', ') }</Text>
+    <Text className='text-lg font-bold text-center'>{ props.title }</Text>
+    { props.subtitle && <Text className='text-lg font-bold text-center'>{ props.subtitle }</Text> }
+    <Text className='text-lg font-bold text-center'>{ props.authors.join(', ') }</Text>
   </View>
 )
 
