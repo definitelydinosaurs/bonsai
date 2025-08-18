59f513faf434e7b9a71078662b0ce99d9175a16c
arandombit <a.random.luis@proton.me>
2025-08-17 23:26:33 -0400

    display shelves in shelves screen

--- src/app/shelves.jsx
+++ src/app/shelves.jsx
@@ -13,6 +13,13 @@ export default function Settings() {
   return (
     <View className='flex-1 p-4'>
       <Text className='text-2xl font-bold mb-4'>Shelves</Text>
+      <ScrollView contentContainerClassName='w-full grid grid-cols-3 gap-4' showsVerticalScrollIndicator={false}>
+        { Object.keys(data.shelves).map(shelf => (
+          <View key={shelf} className='w-full justify-center items-center mb-4'>
+            <Text>{ shelf }</Text>
+          </View>
+        )) }
+      </ScrollView>
     </View>
   )
 } 
