c1db213ae3623bbcd49da6bf10340e1eca69c62a
Randy Brown <thatrandybrown@gmail.com>
2026-03-08 21:21:29 -0500

    update layout to setup tamagui

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,26 @@
 import { Stack } from "expo-router";
+import { useColorScheme } from "react-native";
+import { TamaguiProvider, Theme } from "tamagui";
+
+import { config } from "@tamagui/config";
+import { createTamagui } from "tamagui";
+
+const tamaguiConfig = createTamagui(config);
+
+type Conf = typeof tamaguiConfig;
+
+declare module "tamagui" {
+  interface TamaguiCustomConfig extends Conf {}
+}
 
 export default function RootLayout() {
-  return <Stack />;
+  const colorScheme = useColorScheme();
+
+  return (
+    <TamaguiProvider config={tamaguiConfig} defaultTheme={colorScheme!}>
+      <Theme name="dark">
+        <Stack screenOptions={{ headerShown: false }} />
+      </Theme>
+    </TamaguiProvider>
+  );
 }
