f944794bef89daa5223c6c86f1747bcc5f9eb1d0
Randy Brown <thatrandybrown@gmail.com>
2026-03-13 21:31:44 -0500

    if there are search results, show them

--- app/index.tsx
+++ app/index.tsx
@@ -58,7 +58,7 @@ export default function Index() {
         {(searchResults || activeSource) && (
           <BaseDetails
             node={{
-              ...(sources && (sources[activeSource ?? ""] ?? {})),
+              ...(searchResults ? searchResults : sources && (sources[activeSource ?? ""] ?? {})),
             }}
           />
         )}
