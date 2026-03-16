e69a59b24b700964f4b24aeae43546ad84f2fefc
Randy Brown <thatrandybrown@gmail.com>
2026-03-15 20:57:06 -0500

    key on the correct attribute

--- app/index.tsx
+++ app/index.tsx
@@ -106,7 +106,7 @@ export default function Index() {
               },
             },
           }))}
-          targetKey="slug"
+          targetKey="title"
           width="100%"
         />
       )}
