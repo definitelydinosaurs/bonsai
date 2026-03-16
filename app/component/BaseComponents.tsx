585777ef650f1a6ae5b948b10e5211bfd17a94df
Randy Brown <thatrandybrown@gmail.com>
2026-03-15 20:56:54 -0500

    text needs a tag

--- app/component/BaseComponents.tsx
+++ app/component/BaseComponents.tsx
@@ -269,7 +269,7 @@ export const BaseDetails = ({ node }: { node: Record<string, unknown> }) => {
   return (
     <>
       {Object.entries(node ?? {}).filter(([key, value]) => value !== undefined && value !== null && typeof value === "string").map(([key, value]) => (
-        <ListItem key={key}>{`${key}: ${value}`}</ListItem>
+        <ListItem key={key}><Text>{`${key}: ${value}`}</Text></ListItem>
       ))}
     </>
   );
